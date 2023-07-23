893bf3c966e4ad22bdf525473babbc4d5aa990ad
Gabriel-Rocha <gabriel.santos15053@gmail.com>
2023-07-23 01:09:27 -0300

    fix: add .env e trocando a config da chamada

--- index.js
+++ index.js
@@ -2,45 +2,48 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
 const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
+const dotenv = require('dotenv');
 
 const csvFilePath = path.join(__dirname, 'dados_csv', 'meuCSV.csv');
 
+dotenv.config(); // Carrega as variáveis de ambiente do .env
+
 const jsonArray = [];
 
+console.log('Iniciando leitura do arquivo CSV...');
+
 fs.createReadStream(csvFilePath)
-  .pipe(csv({ separator: ';' })) // Set the separator to semicolon
+  .pipe(csv({ separator: ';' })) // Defina o separador como ponto e vírgula
   .on('data', (data) => {
     jsonArray.push(data);
   })
   .on('end', async () => {
-    // Convert 'Storage Used (Byte)' to gigabytes for each object in the array
+    console.log('Leitura do arquivo CSV concluída.');
+    // Converta 'Storage Used (Byte)' para gigabytes para cada objeto no array
     jsonArray.forEach((obj) => {
       if (obj['Storage Used (Byte)']) {
         const bytes = parseFloat(obj['Storage Used (Byte)']);
         const gigabytes = bytes / (1024 * 1024 * 1024); // 1 gigabyte = 1024 MB = 1024 * 1024 KB = 1024 * 1024 * 1024 bytes
-        obj['Storage Used (GB)'] = gigabytes.toFixed(2); // Adding new field with the converted value
+        obj['Storage Used (GB)'] = gigabytes.toFixed(2); // Adicionando novo campo com o valor convertido
       }
     });
 
-    // Filter the objects where 'Storage Used (GB)' reaches 80% of 50GB (40GB)
+    // Filtrar os objetos onde 'Storage Used (GB)' atinge 80% de 50GB (40GB)
     const filteredArray = jsonArray.filter((obj) => {
       return obj['Storage Used (GB)'] >= 40;
     });
 
     if (filteredArray.length > 0) {
-      // Get Ethereal test account credentials
-      const testAccount = await nodemailer.createTestAccount();
-
-      // Sending the filtered JSON data via email for each object that meets the condition
-      const transporter = nodemailer.createTransport(smtpTransport({
-        host: 'smtp.ethereal.email',
+      console.log('Iniciando envio de e-mails...');
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com', // Servidor SMTP do Microsoft 365
         port: 587,
+        secure: false, // A conexão não é segura, pois o porto é 587
         auth: {
-          user: testAccount.user, // Use the Ethereal email address as the 'from' field
-          pass: testAccount.pass, // Use the Ethereal email password
+          user: process.env.EMAIL_USER, // Use o seu e-mail aqui
+          pass: process.env.EMAIL_PASS, // Use a senha do seu e-mail aqui
         },
-      }));
+      });
 
       filteredArray.forEach((obj) => {
         // Get the current date
@@ -54,22 +57,28 @@ fs.createReadStream(csvFilePath)
         const formattedDate = `${nextDay.getDate()}/${nextDay.getMonth() + 1}/${nextDay.getFullYear()}`;
         const clinteName = obj['Display Name'];
         const mailOptions = {
-          from: testAccount.user, // Use the Ethereal email address as the 'from' field
-          to: obj['User Principal Name'], // Use 'User Principal Name' as the recipient's email address
-          subject: 'Filtered JSON Data',
-          text: `Deu certo ${formattedDate} ${clinteName}`, // Modify the email body to include the text and date
+          from: process.env.EMAIL_USER, // Use o seu endereço de e-mail aqui
+          to: obj['User Principal Name'], // Use 'User Principal Name' como o destinatário do e-mail
+          subject: 'Dados filtrados do JSON',
+          text: `
+            Olá, ${clinteName}, notamos que o limite de espaço em disco do seu e-mail está se aproximando. 
+            Agendamos uma limpeza para o dia ${formattedDate}.
+            
+            Atenciosamente, Gabriel Rocha`, // Modifique o corpo do e-mail para incluir o texto e a data
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
-            console.log('Error occurred while sending email:', error);
+            console.log('Ocorreu um erro ao enviar o e-mail:', error);
           } else {
-            console.log('Email sent:', info.response);
-            console.log('Ethereal Email URL:', nodemailer.getTestMessageUrl(info));
+            console.log('E-mail enviado:', info.response);
+            console.log('URL do E-mail Ethereal:', nodemailer.getTestMessageUrl(info));
           }
         });
       });
+
+      console.log('Envio de e-mails concluído.');
     } else {
-      console.log("No objects meet the condition: 'Storage Used (GB)' >= 40GB (80% of 50GB).");
+      console.log("Nenhum objeto atende à condição: 'Storage Used (GB)' >= 40GB (80% de 50GB).");
     }
   });
